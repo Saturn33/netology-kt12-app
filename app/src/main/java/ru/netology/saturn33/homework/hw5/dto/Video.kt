0db8f5784ad6461f03048ccc097e6658806cdf32
Vladimir Korkin <vladimir@kork.in>
2019-09-18 22:59:23 +0300

    HW5: закомментированы ненужные пока свойства

--- app/src/main/java/ru/netology/saturn33/homework/hw5/dto/Video.kt
+++ app/src/main/java/ru/netology/saturn33/homework/hw5/dto/Video.kt
@@ -2,8 +2,10 @@ package ru.netology.saturn33.homework.hw5.dto
 
 class Video(
     val id: Int,
-    val url: String,//ссылка на контент
+    val url: String//ссылка на контент
+/*
     val caption: String?,//подпись
     var likes: Int = 0,//количество лайков
     val duration: Int = 0//длительность видео
+*/
 )
